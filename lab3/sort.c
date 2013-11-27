686d742e52b5f7d6b3fb55b1ed72f4ed530caf24
Emil <karlemil.lindberg@gmail.com>
2013-11-27 15:23:57 +0100

    Implemented pivot for three elements

--- lab3/sort.c
+++ lab3/sort.c
@@ -23,6 +23,9 @@
 
 // Do not touch or move these lines
 #include <stdio.h>
+#include "stdlib.h"
+#include "math.h"
+#include "time.h"
 #include "disable.h"
 
 #ifndef DEBUG
@@ -33,11 +36,62 @@
 #include "sort.h"
 #include "simple_quicksort.h"
 
+#define INT_MAX 2147483647
+#define INT_MIN -2147483647
+
+inline int
+fetch_and_add(int *ptr, int value)
+{
+	return __sync_fetch_and_add(ptr, value);
+}
+
 int
 sort(struct array * array)
 {
+	srand(time(NULL));
 	simple_quicksort_ascending(array);
 
 	return 0;
 }
 
+void
+calculate_pivot_3(struct * array, int *pivot_low, int *pivot_high)
+{
+	int max, min, average;
+	min = INT_MAX;
+	max = INT_MIN;
+
+	int length = array->length;
+	int n = (int)sqrt(array->length);
+
+	for (int i = 0; i < n; ++i)
+	{
+		int current_value = array->data[random(length)]
+		average += current_value;
+
+		if (current_value < min)
+			min = current_value;
+		if (current_value > max)
+			max = current_value;
+	}
+
+	average /= n;
+
+	pivot_low = (min + average) / 2;
+	pivot_high = (max + average) / 2;
+}
+
+int
+random(int max)
+{
+	int high;
+	int low;
+
+	low = rand();
+	high = rand();
+
+	high = high << 16;
+	high = high | low;
+
+	return high % max;
+}
