c68a3d1b99b4582d73d4457364f1e11d60e36c03
Emil <karlemil.lindberg@gmail.com>
2013-11-28 08:51:26 +0100

    Changes

--- lab3/sort.h
+++ lab3/sort.h
@@ -4,15 +4,11 @@
 #define NDEBUG
 #endif
 
-#ifndef SORT
-#define SORT
+#ifndef MERGE_SORT
+#define MERGE_SORT
 
 int sort(struct array *);
 
-inline int fetch_and_add(int *ptr, int value);
-void calculate_pivot_3(const struct array * array, int *pivot_low, int *pivot_high);
-void calculate_pivot(const struct array * array, int *pivot);
-int random_int(int max);
-void insertion_sort(struct array * array);
-void parallell_quicksort(struct array *array, int threads);
+void parallell_merge_sort(struct array * array);
+
 #endif
