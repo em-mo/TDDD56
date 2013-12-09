31ea5786944d9a06761f73124e978b6636188dd4
Filip Källström <filip.kallstrom@gmail.com>
2013-12-09 18:38:31 +0100

    fixed errors

--- lab5/ppmfilter.cu
+++ lab5/ppmfilter.cu
@@ -8,9 +8,13 @@
 	#include <GL/glut.h>
 #endif
 
+#define BLOCKSIZE 16
+#define SHAREDMEMSIZE BLOCKSIZE+4
+
+__device__
 inline
 void 
-setPixel(unsigned char *shared_image, unsigned char *image, int shared_index, int image_index)
+set_pixel(unsigned char *shared_image, unsigned char *image, int shared_index, int image_index)
 { 
 	shared_image[shared_index+0] = image[image_index+0];
 	shared_image[shared_index+1] = image[image_index+1];
@@ -21,12 +25,6 @@ __global__ void filter(unsigned char *image, unsigned char *out, int n, int m)
 {
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	int j = blockIdx.y * blockDim.y + threadIdx.y;
-
-	int local_i = threadIdx.x + 2;
-	int local_j = threadIdx.y + 2;
-
-	int global_index = (i*n+j) * 3;
-	int local_index = (local_i*sharedDimX+local_j) * 3;
 
 	int sumx, sumy, sumz, k, l;
 
@@ -66,9 +64,15 @@ __global__ void filter_shared(unsigned char *image, unsigned char *out, int n, i
 	int sharedDimX = blockDim.x + 4;
 	int sharedDimY = blockDim.y + 4;
 // printf is OK under --device-emulation
-//	printf("%d %d %d %d\n", i, j, n, m);
 
-	__shared__ unsigned char shared_image[sharedDimX*sharedDimY*3];
+//	printf("%d %d %d %d\n", i, j, n, m);
+	int local_i = threadIdx.x + 2;
+	int local_j = threadIdx.y + 2;
+
+	int global_index = (i*n+j) * 3;
+	int local_index = (local_i*sharedDimX+local_j) * 3;
+
+	__shared__ unsigned char shared_image[SHAREDMEMSIZE*SHAREDMEMSIZE*3];
 
 	if (j < n && i < m)
 	{
@@ -78,63 +82,63 @@ __global__ void filter_shared(unsigned char *image, unsigned char *out, int n, i
 	// Top
 	if (threadIdx.y == 0 && blockIdx.y != 0)
 	{
-		set_pixel(shared_image, image, local_i-(3*sharedDimX), global_index-(3*n));
-		set_pixel(shared_image, image, local_i-(6*sharedDimX), global_index-(6*n));
+		set_pixel(shared_image, image, local_index-(3*sharedDimX), global_index-(3*n));
+		set_pixel(shared_image, image, local_index-(6*sharedDimX), global_index-(6*n));
 
 		// Upper left
 		if (threadIdx.x == 0 && blockIdx.x != 0)
 		{
-			set_pixel(shared_image, image, local_i-(3*sharedDimX)-3, global_index-(3*n)-3);
-			set_pixel(shared_image, image, local_i-(3*sharedDimX)-6, global_index-(3*n)-6);
+			set_pixel(shared_image, image, local_index-(3*sharedDimX)-3, global_index-(3*n)-3);
+			set_pixel(shared_image, image, local_index-(3*sharedDimX)-6, global_index-(3*n)-6);
 
-			set_pixel(shared_image, image, local_i-(6*sharedDimX)-3, global_index-(6*n)-3);
-			set_pixel(shared_image, image, local_i-(6*sharedDimX)-6, global_index-(6*n)-6);
+			set_pixel(shared_image, image, local_index-(6*sharedDimX)-3, global_index-(6*n)-3);
+			set_pixel(shared_image, image, local_index-(6*sharedDimX)-6, global_index-(6*n)-6);
 		}
 		// Upper right
 		else if (threadIdx.x == blockDim.x - 1  && blockIdx.x != gridDim.x - 1)
 		{
-			set_pixel(shared_image, image, local_i-(3*sharedDimX)+3, global_index-(3*n)+3);
-			set_pixel(shared_image, image, local_i-(3*sharedDimX)+6, global_index-(3*n)+6);
+			set_pixel(shared_image, image, local_index-(3*sharedDimX)+3, global_index-(3*n)+3);
+			set_pixel(shared_image, image, local_index-(3*sharedDimX)+6, global_index-(3*n)+6);
 
-			set_pixel(shared_image, image, local_i-(6*sharedDimX)+3, global_index-(6*n)+3);
-			set_pixel(shared_image, image, local_i-(6*sharedDimX)+6, global_index-(6*n)+6);
+			set_pixel(shared_image, image, local_index-(6*sharedDimX)+3, global_index-(6*n)+3);
+			set_pixel(shared_image, image, local_index-(6*sharedDimX)+6, global_index-(6*n)+6);
 		}
 	}
 	if (threadIdx.y == blockDim.y - 1 && blockIdx.y != gridDim.y - 1)
 	{
-		set_pixel(shared_image, image, local_i+(3*sharedDimX), global_index+(3*n));
-		set_pixel(shared_image, image, local_i+(6*sharedDimX), global_index+(6*n));
+		set_pixel(shared_image, image, local_index+(3*sharedDimX), global_index+(3*n));
+		set_pixel(shared_image, image, local_index+(6*sharedDimX), global_index+(6*n));
 		// Lower left
 		if (threadIdx.x == 0 && blockIdx.x != 0)
 		{
-			set_pixel(shared_image, image, local_i+(3*sharedDimX)-3, global_index+(3*n)-3);
-			set_pixel(shared_image, image, local_i+(3*sharedDimX)-6, global_index+(3*n)-6);
+			set_pixel(shared_image, image, local_index+(3*sharedDimX)-3, global_index+(3*n)-3);
+			set_pixel(shared_image, image, local_index+(3*sharedDimX)-6, global_index+(3*n)-6);
 
-			set_pixel(shared_image, image, local_i+(6*sharedDimX)-3, global_index+(6*n)-3);
-			set_pixel(shared_image, image, local_i+(6*sharedDimX)-6, global_index+(6*n)-6);
+			set_pixel(shared_image, image, local_index+(6*sharedDimX)-3, global_index+(6*n)-3);
+			set_pixel(shared_image, image, local_index+(6*sharedDimX)-6, global_index+(6*n)-6);
 		}
 		// Lower right
 		else if (threadIdx.x == blockDim.x - 1 && blockIdx.x != gridDim.x - 1)
 		{
-			set_pixel(shared_image, image, local_i+(3*sharedDimX)+3, global_index+(3*n)+3);
-			set_pixel(shared_image, image, local_i+(3*sharedDimX)+6, global_index+(3*n)+6);
+			set_pixel(shared_image, image, local_index+(3*sharedDimX)+3, global_index+(3*n)+3);
+			set_pixel(shared_image, image, local_index+(3*sharedDimX)+6, global_index+(3*n)+6);
 
-			set_pixel(shared_image, image, local_i+(6*sharedDimX)+3, global_index+(6*n)+3);
-			set_pixel(shared_image, image, local_i+(6*sharedDimX)+6, global_index+(6*n)+6);
+			set_pixel(shared_image, image, local_index+(6*sharedDimX)+3, global_index+(6*n)+3);
+			set_pixel(shared_image, image, local_index+(6*sharedDimX)+6, global_index+(6*n)+6);
 		
 		}
 	}
 	// Left
 	if (threadIdx.x == 0 && blockIdx.x != 0)
 	{
-		set_pixel(shared_image, image, local_i-3, global_index-3);
-		set_pixel(shared_image, image, local_i-6, global_index-6);
+		set_pixel(shared_image, image, local_index-3, global_index-3);
+		set_pixel(shared_image, image, local_index-6, global_index-6);
 	}
 	// Right
 	else if (threadIdx.x == blockDim.x - 1  && blockIdx.x != gridDim.x - 1)
 	{
-		set_pixel(shared_image, image, local_i+3, global_index+3);
-		set_pixel(shared_image, image, local_i+6, global_index+6);
+		set_pixel(shared_image, image, local_index+3, global_index+3);
+		set_pixel(shared_image, image, local_index+6, global_index+6);
 	}
 
 	if (i > 1 && i < m-2 && j > 1 && j < n-2)
@@ -188,9 +192,7 @@ void Draw()
     cudaEventSynchronize(end_event);
     
     cudaEventElapsedTime(&theTime, start_event, end_event);
-    printf("The time: %f ms\n", theTime);
-    cout << "The time: " << theTime << " ms" << endl;
-
+    
 	cudaMemcpy( out, dev_out, n*m*3, cudaMemcpyDeviceToHost );
 	cudaFree(dev_image);
 	cudaFree(dev_out);
