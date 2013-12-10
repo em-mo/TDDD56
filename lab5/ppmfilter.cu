5cbebea1e337d05998ccf6becc63463d73cea290
Emil Lindberg <karlemil.lindberg@gmail.com>
2013-12-10 12:46:37 +0100

    Working lab 5, with dual timings

--- lab5/ppmfilter.cu
+++ lab5/ppmfilter.cu
@@ -9,7 +9,7 @@
 #endif
 
 #define BLOCKSIZE 16
-#define SHAREDMEMSIZE BLOCKSIZE+4
+#define SHAREDMEMSIZE (BLOCKSIZE + 4)
 
 __device__
 inline
@@ -70,61 +70,59 @@ __global__ void filter_shared(unsigned char *image, unsigned char *out, int n, i
 	int global_index = (j*n+i) * 3;
 	int local_index = (local_y*SHAREDMEMSIZE+local_x) * 3;
 
-	__shared__ unsigned char shared_image[SHAREDMEMSIZE*SHAREDMEMSIZE*3];
+	__shared__ unsigned char shared_image[SHAREDMEMSIZE*SHAREDMEMSIZE*6];
 	if (j < n && i < m)
 	{
-		// set_pixel(shared_image, image, local_index, global_index);
-		shared_image[local_index+0] = image[global_index+0];
-		shared_image[local_index+1] = image[global_index+1];
-		shared_image[local_index+2] = image[global_index+2];
+		set_pixel(shared_image, image, local_index, global_index);
+		set_pixel(out, shared_image, global_index, local_index);
 	}
 	
 	// Top
 	if (threadIdx.y == 0 && blockIdx.y != 0)
 	{
-		set_pixel(shared_image, image, local_index-(3*sharedDimX), global_index-(3*n));
-		set_pixel(shared_image, image, local_index-(6*sharedDimX), global_index-(6*n));
+		set_pixel(shared_image, image, local_index-(3*SHAREDMEMSIZE), global_index-(3*n));
+		set_pixel(shared_image, image, local_index-(6*SHAREDMEMSIZE), global_index-(6*n));
 
 		// Upper left
 		if (threadIdx.x == 0 && blockIdx.x != 0)
 		{
-			set_pixel(shared_image, image, local_index-(3*sharedDimX)-3, global_index-(3*n)-3);
-			set_pixel(shared_image, image, local_index-(3*sharedDimX)-6, global_index-(3*n)-6);
+			set_pixel(shared_image, image, local_index-(3*SHAREDMEMSIZE)-3, global_index-(3*n)-3);
+			set_pixel(shared_image, image, local_index-(3*SHAREDMEMSIZE)-6, global_index-(3*n)-6);
 
-			set_pixel(shared_image, image, local_index-(6*sharedDimX)-3, global_index-(6*n)-3);
-			set_pixel(shared_image, image, local_index-(6*sharedDimX)-6, global_index-(6*n)-6);
+			set_pixel(shared_image, image, local_index-(6*SHAREDMEMSIZE)-3, global_index-(6*n)-3);
+			set_pixel(shared_image, image, local_index-(6*SHAREDMEMSIZE)-6, global_index-(6*n)-6);
 		}
 		// Upper right
 		else if (threadIdx.x == blockDim.x - 1  && blockIdx.x != gridDim.x - 1)
 		{
-			set_pixel(shared_image, image, local_index-(3*sharedDimX)+3, global_index-(3*n)+3);
-			set_pixel(shared_image, image, local_index-(3*sharedDimX)+6, global_index-(3*n)+6);
+			set_pixel(shared_image, image, local_index-(3*SHAREDMEMSIZE)+3, global_index-(3*n)+3);
+			set_pixel(shared_image, image, local_index-(3*SHAREDMEMSIZE)+6, global_index-(3*n)+6);
 
-			set_pixel(shared_image, image, local_index-(6*sharedDimX)+3, global_index-(6*n)+3);
-			set_pixel(shared_image, image, local_index-(6*sharedDimX)+6, global_index-(6*n)+6);
+			set_pixel(shared_image, image, local_index-(6*SHAREDMEMSIZE)+3, global_index-(6*n)+3);
+			set_pixel(shared_image, image, local_index-(6*SHAREDMEMSIZE)+6, global_index-(6*n)+6);
 		}
 	}
-	if (threadIdx.y == blockDim.y - 1 && blockIdx.y != gridDim.y - 1)
+	else if (threadIdx.y == blockDim.y - 1 && blockIdx.y != gridDim.y - 1)
 	{
-		set_pixel(shared_image, image, local_index+(3*sharedDimX), global_index+(3*n));
-		set_pixel(shared_image, image, local_index+(6*sharedDimX), global_index+(6*n));
+		set_pixel(shared_image, image, local_index+(3*SHAREDMEMSIZE), global_index+(3*n));
+		set_pixel(shared_image, image, local_index+(6*SHAREDMEMSIZE), global_index+(6*n));
 		// Lower left
 		if (threadIdx.x == 0 && blockIdx.x != 0)
 		{
-			set_pixel(shared_image, image, local_index+(3*sharedDimX)-3, global_index+(3*n)-3);
-			set_pixel(shared_image, image, local_index+(3*sharedDimX)-6, global_index+(3*n)-6);
+			set_pixel(shared_image, image, local_index+(3*SHAREDMEMSIZE)-3, global_index+(3*n)-3);
+			set_pixel(shared_image, image, local_index+(3*SHAREDMEMSIZE)-6, global_index+(3*n)-6);
 
-			set_pixel(shared_image, image, local_index+(6*sharedDimX)-3, global_index+(6*n)-3);
-			set_pixel(shared_image, image, local_index+(6*sharedDimX)-6, global_index+(6*n)-6);
+			set_pixel(shared_image, image, local_index+(6*SHAREDMEMSIZE)-3, global_index+(6*n)-3);
+			set_pixel(shared_image, image, local_index+(6*SHAREDMEMSIZE)-6, global_index+(6*n)-6);
 		}
 		// Lower right
 		else if (threadIdx.x == blockDim.x - 1 && blockIdx.x != gridDim.x - 1)
 		{
-			set_pixel(shared_image, image, local_index+(3*sharedDimX)+3, global_index+(3*n)+3);
-			set_pixel(shared_image, image, local_index+(3*sharedDimX)+6, global_index+(3*n)+6);
+			set_pixel(shared_image, image, local_index+(3*SHAREDMEMSIZE)+3, global_index+(3*n)+3);
+			set_pixel(shared_image, image, local_index+(3*SHAREDMEMSIZE)+6, global_index+(3*n)+6);
 
-			set_pixel(shared_image, image, local_index+(6*sharedDimX)+3, global_index+(6*n)+3);
-			set_pixel(shared_image, image, local_index+(6*sharedDimX)+6, global_index+(6*n)+6);
+			set_pixel(shared_image, image, local_index+(6*SHAREDMEMSIZE)+3, global_index+(6*n)+3);
+			set_pixel(shared_image, image, local_index+(6*SHAREDMEMSIZE)+6, global_index+(6*n)+6);
 		
 		}
 	}
@@ -180,15 +178,31 @@ void Draw()
     cudaEventCreate(&end_event);
 	cudaMalloc( (void**)&dev_image, n*m*3);
 	cudaMalloc( (void**)&dev_out, n*m*3);
+	cudaMemset(dev_out, 0, n*m*3);
 	cudaMemcpy( dev_image, image, n*m*3, cudaMemcpyHostToDevice);
 	
 	dim3 dimBlock( 16, 16 );
 	dim3 dimGrid( 32, 32 );
+
+	dim3 ourBlock( BLOCKSIZE, BLOCKSIZE );
+	dim3 ourGrid( n / BLOCKSIZE, m / BLOCKSIZE );
+
+    cudaEventRecord(start_event, 0);
+    cudaEventSynchronize(start_event);
+
+	filter<<<ourGrid, ourBlock>>>(dev_image, dev_out, n, m);
+	cudaThreadSynchronize();
+	
+    cudaEventRecord(end_event, 0);
+    cudaEventSynchronize(end_event);
+    
+    cudaEventElapsedTime(&theTime, start_event, end_event);
+    printf("Slow time: %f ms\n", theTime);
 	
     cudaEventRecord(start_event, 0);
     cudaEventSynchronize(start_event);
 
-	filter_shared<<<dimGrid, dimBlock>>>(dev_image, dev_out, n, m);
+	filter_shared<<<ourGrid, ourBlock>>>(dev_image, dev_out, n, m);
 	cudaThreadSynchronize();
 	
     cudaEventRecord(end_event, 0);
